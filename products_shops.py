944470f03e6b866807f46ec77fab7a0cc43df0a0
Maksym Sukhorukov <makskant@gmail.com>
2017-01-13 02:20:02 +0200

    Update products_shops.py

--- products_shops.py
+++ products_shops.py
@@ -1,4 +1,6 @@
 class Products_shops:
-    def __init__(self, name, products):
+    def __init__(self, name, products,x_coord,y_coord):
         self.name = name
         self.products = products
+        self.x = x_coord
+        self.y = y_coord
